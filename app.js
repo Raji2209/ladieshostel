584fce127a37cf476d0d47122cb9f4eb84cfbe35
sudharsan.tk <sudharsan_tk@yahoo.co.in>
2016-10-12 17:17:19 +0800

    push

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('nodemailer');
 var nodemailer = require('nodemailer');
- var routes = require('./routes/index');
+var routes = require('./routes/index');
 
 var app = express();
  
@@ -22,11 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
- 
- 
-
-
 
  
 // create reusable transporter object using the default SMTP transport 
@@ -62,8 +57,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var nodemailer = require('nodemailer');
 
 
 router.get('/',function(req,res,next){
-   res.render('index');
-
+    console.log("entering rot");
+   res.render('index',{title:"Welcome page"});
 });
 
 /**
@@ -29,13 +29,15 @@ router.post('/send', function (req, res, next) {
         // send mail with defined transport object 
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
+                res.render('index',{msg:"Unable to send email,please try again later.",name:req.body.name,error:true});
                 return console.log(error);
             }
-            res.render('index' ,{msg:"sent succ"});
-            console.log('Message sent: ' + info.response);
+            res.render('index',{msg:"Thanks for contacting,we will respond to you shortly.",name:req.body.name});
+             
         });
     } catch (e) {
         console.log(" error " + e);
+        res.render('index',{msg:"Unable to send email,please try again later.",name:req.body.name,error:true});
     }
 });
 
