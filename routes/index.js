08be64b36b4fe29ad0bd0c30cd0223a3cfb59da8
Raji2209 <raji.contactable@gmail.com>
2016-10-14 23:18:04 +0800

    email try with var

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,12 @@ var content = req.body.message;
 var emailadd = req.body.email;
     console.log(" entering ");
     try {
-		var MAILID = ENV['EMAIL_USER'];
-		var PWD = ENV['EMAIL_PASSWORD'];
         //var transporter = nodemailer.createTransport('smtps://testheroku2%40gmail.com:(Test)1234@smtp.gmail.com');
 		var transporter = nodemailer.createTransport({
 			service : 'Gmail',
 			auth: {
-				:user => MAILID,
-				:pass => PWD
+				:user => ENV['EMAIL_USER'],
+				:pass => ENV['EMAIL_PASSWORD']
 			}
 		});
         // setup e-mail data with unicode symbols 
