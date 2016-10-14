54161d848eeca8628a1040a50ed69cb9fad1d475
Raji2209 <raji.contactable@gmail.com>
2016-10-15 00:19:22 +0800

    trying

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/',function(req,res,next){
 * send contact email
 */
 router.post('/send', function (req, res, next) {
-var name = req.body.name;
+var name = process.env.EMAIL_USER;
 var content = req.body.message;
 var emailadd = req.body.email;
     console.log(" entering ");
