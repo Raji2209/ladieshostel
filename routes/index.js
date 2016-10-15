2fab025a4070af5cd53661a274ad6baea6c9dc39
Raji2209 <raji.contactable@gmail.com>
2016-10-16 06:36:07 +0800

    try now

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.post('/send', function (req, res, next) {
 	var name = req.body.name;
 	var content = req.body.message;
 	var emailadd = req.body.email;
+	var issuccess = 0;
     console.log(" entering ");
     try {
         //var transporter = nodemailer.createTransport('smtps://testheroku2%40gmail.com:(Test)1234@smtp.gmail.com');
@@ -40,10 +41,12 @@ router.post('/send', function (req, res, next) {
         // send mail with defined transport object 
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
+				issuccess = 0;
                 res.render('index',{msg:"Unable to send email,please try again later.",name:req.body.name,error:true});
                 return console.log(error);
             }
             res.render('index',{msg:"Thanks for contacting,we will respond to you shortly.",name:req.body.name});
+			issuccess = 1;
              
         });
     } catch (e) {
